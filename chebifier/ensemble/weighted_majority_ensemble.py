fd0b633a25b2bf68ec5adf522518e5f6d8f33500
sfluegel <simon.fluegel@uni-osnabrueck.de>
2025-07-07 12:27:44 +0200

    make f1-weighting more gentle (multiply by 1 + f1 instead of only f1)

--- chebifier/ensemble/weighted_majority_ensemble.py
+++ chebifier/ensemble/weighted_majority_ensemble.py
@@ -36,7 +36,7 @@ class WMVwithF1Ensemble(BaseEnsemble):
         """
         Given the positions of predicted classes in the predictions tensor, assign weights to each class. The
         result is two tensors of shape (num_predicted_classes, num_models). The weight for each class is the model_weight
-        (default: 1) multiplied by the class-specific validation-f1 (default 1).
+        (default: 1) multiplied by (1 + the class-specific validation-f1 (default 1)).
         """
         weights_by_cls = torch.ones(len(predicted_classes), len(self.models))
         for j, model in enumerate(self.models):
@@ -46,7 +46,7 @@ class WMVwithF1Ensemble(BaseEnsemble):
             for cls, weights in model.classwise_weights.items():
                 if (2 * weights["TP"] + weights["FP"] + weights["FN"]) > 0:
                     f1 = 2 * weights["TP"] / (2 * weights["TP"] + weights["FP"] + weights["FN"])
-                    weights_by_cls[predicted_classes[cls], j] *= f1
+                    weights_by_cls[predicted_classes[cls], j] *= 1 + f1
 
         print(f"Calculated model weightings. The average weights are:")
         for i, model in enumerate(self.models):
